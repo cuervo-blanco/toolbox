23878ab30bc9f1c9fa7df301f7b62775892f508c
cuervo-blanco <jaios.228@gmail.com>
2024-11-01 12:20:19 -0400

    mf update

--- scripts/mf.sh
+++ scripts/mf.sh
@@ -2,45 +2,36 @@
 
 FILES=()
 DESTINATION=""
-DIRECTORY=false
-NUMBER_OF_ARGUMENTS=0
-ARGUMENTS=("$@")  # Store all arguments in an array
-
-for arg in "$@"; do
-    ((NUMBER_OF_ARGUMENTS++))
-done
+OPERATION="move"  # Default operation
 
 while [[ "$#" -gt 0 ]]; do
-    case $1 in
+    case "$1" in
         -h|--help)
             echo "Usage:"
-            echo "mf -f file -d destination"
-            echo "use -r if file directory"
+            echo "mf -f file1 file2 ... -d destination [-c]"
+            echo "-c or --copy to copy files/directories instead of moving"
             exit 0
             ;;
         -f|--file)
             shift
             while [[ "$1" && "$1" != -* ]]; do
-                FILES+=("$1")  # Collect files following -f
+                FILES+=("$1")
                 shift
             done
-            continue
             ;;
         -d|--destination)
-            DESTINATION="$2"
             shift
-            ;;
-        -r)
-            DIRECTORY=true
-            ;;
-        -rf | -fr )
-            DIRECTORY=true
-            shift
-            while [[ "$1" && "$1" != -* ]]; do
-                FILES+=("$1")  # Collect files following -f
+            if [[ "$1" && "$1" != -* ]]; then
+                DESTINATION="$1"
                 shift
-            done
-            continue
+            else
+                echo "Error: Destination directory is missing."
+                exit 1
+            fi
+            ;;
+        -c|--copy)
+            OPERATION="copy"
+            shift
             ;;
         --)  # End of all options
             shift
@@ -49,30 +40,31 @@ while [[ "$#" -gt 0 ]]; do
         -*|--*)  # Catch-all for unrecognized options
             echo "Sorry, option $1 is not recognized."
             echo "Usage: mf -f file -d destination"
+            exit 1
             ;;
         *)  # Any remaining arguments (not expected in this context)
             echo "Sorry, argument $1 is not recognized."
             echo "Usage: mf -f file -d destination"
+            exit 1
             ;;
     esac
-    shift  # Move to the next argument
 done
 
 # Check if destination and files are specified
 if [[ -z "$DESTINATION" || ${#FILES[@]} -eq 0 ]]; then
     echo "Error: Destination and at least one file must be specified."
     echo "Usage: mf -f file -d destination"
+    echo "mf -h or --help for help."
     exit 1
 fi
 
-# Move files to destination
-if [[ "$DIRECTORY" == true ]]; then
-    for file in "${FILES[@]}"; do
+# Perform the operation
+for file in "${FILES[@]}"; do
+    if [[ "$OPERATION" == "copy" ]]; then
+        cp -r "$file" "$DESTINATION"
+    else
         mv "$file" "$DESTINATION"
-    done
-else
-    for file in "${FILES[@]}"; do
-        mv "$file" "$DESTINATION"
-    done
-fi
+    fi
+done
+exit 0
 
